1b8faf9476ebac41d7342d4b6d251ec8e820d89a
dupuyivan <dupuyivan8@gmail.com>
2021-05-26 17:07:57 -0300

    folders,server,database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const router = require("./routes/index")
-require(".config/database")
+require("./config/database")
 
 const app = express()
 
