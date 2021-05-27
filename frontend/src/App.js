c0553013cc9c35c6d7712470d5f56b93f8ef0a5b
kevinAsk47 <kevinmunozmendez@gmail.com>
2021-05-27 18:03:46 -0300

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
 import { ToastContainer } from 'react-toastify';
+import Product from "./pages/Product";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/product" component={Product} />
         <Route exact path="/" component={Home} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/signin" component={SignIn} />
