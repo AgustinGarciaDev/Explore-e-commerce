feaf1ca7eeada2b31ffdf253f0654a4368bb0782
kevinAsk47 <kevinmunozmendez@gmail.com>
2021-06-05 23:57:32 -0300

    arreglo cositas de home

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -118,7 +118,7 @@ const userActions = {
 
     sendMail: (mail) => {
         return async (dispatch, getState) => {
-            const response = await axios.post("http://localhost:4000/api/mails/sendDiscount", {mail: mail})
+            const response = await axios.post("https://explore-2021.herokuapp.com/api/mails/sendDiscount", {mail: mail})
             console.log(response)
             return (response)
         }
