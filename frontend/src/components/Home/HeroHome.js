1c058830f1b5c7101bea9c9e6c27f75778984d3e
SantiSarmiento <santisarmiento54@gmail.com>
2021-06-11 11:12:34 -0300

    ignorar

--- frontend/src/components/Home/HeroHome.js
+++ frontend/src/components/Home/HeroHome.js
@@ -11,7 +11,7 @@ const HeroHome = () => {
     return (
         <div className="contenedor">
             <video id="video_hero" loop autoPlay muted>
-                <source src="http://baravdg.com/wp-content/uploads/2021/05/pexels-ron-lach-6756046-1.mp4" type="video/mp4" />
+                <source src="https://baravdg.com/wp-content/uploads/2021/05/pexels-ron-lach-6756046-1.mp4" type="video/mp4" />
             </video>
             <div className="contenido">
                 <h1 className="titleHero">Explore</h1>
